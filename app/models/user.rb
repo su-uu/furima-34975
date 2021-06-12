8cf29a9238bc7722fdf35cff212c6269e45f3163
su-uu <suzutsuba1312128@gmail.com>
2021-06-12 11:32:53 +0900

    正規表現によるバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname, presence: true
+    # 半角英数字が含まれる６字以上の場合のみ許可する
+    validates :password, format: {with: /\A[a-z0-9]+\z/i	, message: "is invalid. Include both letters and numbers."}
 
+    # ひらがな、カタカナ、漢字のみ許可する
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+
+    # カタカナのみ許可する
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+
+    validates :birthday, presence: true
+  end
 end
+
+  # passwordのバリデーションの設定は下記の表記でも可能
+  # validate :password_complexity
+  # def password_complexity
+  #   return if password = /[a-z][\d]/
+  #   errors.add :password, 'is invalid. Include both letters and numbers.'
+  # end
