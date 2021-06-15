183c19da067c1cfc6159e9cfbf7a8294b53c3454
su-uu <suzutsuba1312128@gmail.com>
2021-06-15 23:25:11 +0900

    Productテーブルへのカラムの追加

--- db/migrate/20210615135424_create_products.rb
+++ db/migrate/20210615135424_create_products.rb
@@ -1,6 +1,15 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
+      t.string :title,             null: false
+      t.text :describe,            null: false
+      t.integer :category_id,      null: false
+      t.integer :status_id,        null: false
+      t.integer :delivery_id,      null: false
+      t.integer :prefectures_id,   null: false
+      t.integer :shipping_date_id, null: false
+      t.integer :price,            null: false
+      t.references :user,          foreign_key: true
       t.timestamps
     end
   end
