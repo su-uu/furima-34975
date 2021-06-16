dbbce3af54190c5f07cb474e6ab00496feb8181e
su-uu <suzutsuba1312128@gmail.com>
2021-06-16 21:06:15 +0900

    Productモデルへのバリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+  validates :title            ,presence: true
+  validates :describe         ,presence: true
+  validates :category_id      ,presence: true
+  validates :status_id        ,presence: true
+  validates :delivery_id      ,presence: true
+  validates :prefectures_id   ,presence: true
+  validates :shipping_date_id ,presence: true
+  validates :price            ,presence: true
+  validates :image            , presence: true
+
   belongs_to :user
   has_one_attached :image
 end
