b80eb4fc55dc37de6d5189984d399588b9068918
su-uu <suzutsuba1312128@gmail.com>
2021-06-17 22:26:50 +0900

    ActiveHashの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,17 @@
 class Product < ApplicationRecord
-  validates :title            ,presence: true
-  validates :describe         ,presence: true
-  validates :category_id      ,presence: true
-  validates :status_id        ,presence: true
-  validates :delivery_id      ,presence: true
-  validates :prefectures_id   ,presence: true
-  validates :shipping_date_id ,presence: true
-  validates :price            ,presence: true
-  validates :image            , presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  #空の投稿を保存できないようにする
+  validates :title, :describe, :price, :image ,presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, :status_id, :delivery_id, :prefectures_id, :shipping_date_id ,presence: true, numericality: { other_than: 1 }
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery
+  belongs_to :prefecture
+  belongs_to :shipping_date
   has_one_attached :image
 end
