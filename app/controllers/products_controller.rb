2f7b5781c395ff55c6b7deaf1cee2c30c0029c3f
su-uu <suzutsuba1312128@gmail.com>
2021-06-18 23:28:46 +0900

    createアクションの定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,10 +7,16 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-   
-  end
+
   private
   def product_params
     params.require(:product).permit(:title, :describe, :category_id, :status_id, :delivery_id, :prefectures_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
