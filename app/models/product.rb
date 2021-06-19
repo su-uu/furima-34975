2ac5ab971119fbbb3e2cbab9f32bb9fa8fc1033f
su-uu <suzutsuba1312128@gmail.com>
2021-06-19 12:20:41 +0900

    エラーメッセージの設定とバリデーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,18 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
+  with_options presence: true do
+
   #空の投稿を保存できないようにする
-  validates :title, :describe, :price, :image ,presence: true
+  validates :image, :title, :describe
+
+  # 300以上、9999999以下の整数を許可する
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "out of setting range"}
 
   #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :status_id, :delivery_id, :prefectures_id, :shipping_date_id ,presence: true, numericality: { other_than: 1 }
+  validates :category_id, :status_id, :delivery_id, :prefectures_id, :shipping_date_id, numericality: { other_than: 1, message: "can't be blank"}
 
+  end
   belongs_to :user
   belongs_to :category
   belongs_to :status
