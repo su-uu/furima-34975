3dc2026240fb654dae13c26ec473f0b4f1778f81
su-uu <suzutsuba1312128@gmail.com>
2021-06-22 22:28:59 +0900

    コードレビュー指摘による修正(商品出品機能)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,27 +24,27 @@ RSpec.describe Product, type: :model do
         expect(@product).to be_valid
       end
       it 'category_idが1以上（--- 以外）であれば保存できること' do
-        @product.category_id = '2'
+        @product.category_id = 2
         expect(@product).to be_valid
       end
       it 'status_idが1以上（--- 以外）であれば保存できること' do
-        @product.status_id = '2'
+        @product.status_id = 2
         expect(@product).to be_valid
       end
       it 'delivery_idが1以上（--- 以外）であれば保存できること' do
-        @product.delivery_id = '2'
+        @product.delivery_id = 2
         expect(@product).to be_valid
       end
       it 'prefectures_idが1以上（--- 以外）であれば保存できること' do
-        @product.prefectures_id = '2'
+        @product.prefectures_id = 2
         expect(@product).to be_valid
       end
       it 'shipping_date_idが1以上（--- 以外）であれば保存できること' do
-        @product.shipping_date_id = '2'
+        @product.shipping_date_id = 2
         expect(@product).to be_valid
       end
       it 'priceが半角数字の300以上,9,999,999以下であれば保存できること' do
-        @product.price = '300'
+        @product.price = 300
         expect(@product).to be_valid
       end
     end
@@ -66,27 +66,27 @@ RSpec.describe Product, type: :model do
         expect(@product.errors.full_messages).to include("Describe can't be blank")
       end
       it 'category_idが1（---）だと保存できないこと' do
-        @product.category_id = '1'
+        @product.category_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Category can't be blank")
       end
       it 'status_idが1（---）だと保存できないこと' do
-        @product.status_id = '1'
+        @product.status_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Status can't be blank")
       end
       it 'delivery_idが1（---）だと保存できないこと' do
-        @product.delivery_id = '1'
+        @product.delivery_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Delivery can't be blank")
       end
       it 'prefectures_idが1（---）だと保存できないこと' do
-        @product.prefectures_id = '1'
+        @product.prefectures_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Prefectures can't be blank")
       end
       it 'shipping_date_idが1（---）だと保存できないこと' do
-        @product.shipping_date_id = '1'
+        @product.shipping_date_id = 1
         @product.valid?
         expect(@product.errors.full_messages).to include("Shipping date can't be blank")
       end
@@ -96,17 +96,27 @@ RSpec.describe Product, type: :model do
         expect(@product.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300円未満だと保存できないこと' do
-        @product.price = '299'
+        @product.price = 299
         @product.valid?
         expect(@product.errors.full_messages).to include('Price out of setting range')
       end
       it 'priceが10,000,000円以上だと保存できないこと' do
-        @product.price = '10000000'
+        @product.price = 10000000
         @product.valid?
         expect(@product.errors.full_messages).to include('Price out of setting range')
       end
       it 'priceが全角数字だと保存できないこと' do
         @product.price = '３００'
+        @product.valid?
+        expect(@product.errors.full_messages).to include('Price is invalid. Input half-width characters')
+      end
+      it 'priceが半角英数字混合では保存できないこと' do
+        @product.price = '300yen'
+        @product.valid?
+        expect(@product.errors.full_messages).to include('Price is invalid. Input half-width characters')
+      end
+      it 'priceが半角英字のみでは保存できないこと' do
+        @product.price = 'senyen'
         @product.valid?
         expect(@product.errors.full_messages).to include('Price is invalid. Input half-width characters')
       end
