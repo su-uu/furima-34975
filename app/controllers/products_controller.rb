1d3056eb38c0e844ea608f633039374996aabf08
su-uu <suzutsuba1312128@gmail.com>
2021-06-26 21:47:18 +0900

    Rubocopによる自動修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,9 +36,9 @@ class ProductsController < ApplicationController
 
   def destroy
     if @product.destroy
-    redirect_to root_path
-    else  
-    redirect_to product_path
+      redirect_to root_path
+    else
+      redirect_to product_path
     end
   end
 
