ed3190e5e9ec2fdeeee7d98c8492ff925f72f291
su-uu <suzutsuba1312128@gmail.com>
2021-07-01 22:27:57 +0900

    アクションとルーティングの設定

---
+++ app/controllers/buys_controller.rb
@@ -0,0 +1,8 @@
+class BuysController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :products
+  resources :buys, only: [:index, :create]
 end
