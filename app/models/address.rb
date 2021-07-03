c3f94c99348074b0bb4334ab75cd437fd3c020bc
su-uu <suzutsuba1312128@gmail.com>
2021-07-03 16:45:01 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,16 @@
 class Address < ApplicationRecord
+
+  with_options presence: true do
+
+    # 3桁の数字と「-」と4桁の数字の組み合わせ以外保存できないようにする
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    
+    # ジャンルの選択が「---」の時は保存できないようにする
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+    # 空の投稿を保存できないようにする
+    validates :city, :street, :tel
+  end
+
   belongs_to :buy
 end
