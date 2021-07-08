1b73316564ec0b48753bbc6ad132d510c6456881
su-uu <suzutsuba1312128@gmail.com>
2021-07-08 22:16:26 +0900

    createアクションの実装（buy・addressテーブルへの保存）

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,11 +1,27 @@
 class BuysController < ApplicationController
   def index
+    @product = Product.find(params[:product_id])
+    @buy_address = BuyAddress.new
   end
 
-  def new
-  end
+  # def new
+  #   @buy_address = BuyAddress.new
+  # end
 
   def create
+    @buy_address = BuyAddress.new(buy_params)
+    # @product = Product.find(params[:product_id])
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      @product = Product.find(params[:product_id])
+      render :index
+    end
   end
-  
+
+  private
+  def buy_params
+    params.permit(:postal_code, :prefecture_id, :city, :street, :building, :tel, :product_id).merge(user_id: current_user.id)
+  end
 end
