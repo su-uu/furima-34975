80024661a6d598f699645c2c2f898ff16e0538f1
su-uu <suzutsuba1312128@gmail.com>
2021-07-10 18:33:50 +0900

    PAYJPの実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,14 +4,11 @@ class BuysController < ApplicationController
     @buy_address = BuyAddress.new
   end
 
-  # def new
-  #   @buy_address = BuyAddress.new
-  # end
-
   def create
     @buy_address = BuyAddress.new(buy_params)
-    # @product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
     if @buy_address.valid?
+      pay_item
       @buy_address.save
       redirect_to root_path
     else
@@ -22,6 +19,15 @@ class BuysController < ApplicationController
 
   private
   def buy_params
-    params.permit(:postal_code, :prefecture_id, :city, :street, :building, :tel, :product_id).merge(user_id: current_user.id)
+    params.permit(:postal_code, :prefecture_id, :city, :street, :building, :tel, :product_id).merge(user_id: current_user.id, token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @product.price,
+        card: buy_params[:token],
+        currency: 'jpy'
+      )
+    end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,14 +1,14 @@
 class BuyAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :tel, :user_id,:product_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street, :building, :tel, :user_id, :product_id, :token
 
   with_options presence: true do
     # 3桁の数字と「-」と4桁の数字の組み合わせ以外保存できないようにする
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     
     # 空の投稿を保存できないようにする
-    validates :city, :street, :tel, :user_id
+    validates :city, :street, :tel, :user_id, :token
   end
   
   # ジャンルの選択が「---」の時は保存できないようにする
